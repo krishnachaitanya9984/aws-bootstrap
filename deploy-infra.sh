3c21f1d0d81089cff6a927c9d81fcba9c2a9752c
krinyny <krishnachaitanya9984@gmail.com>
2025-10-28 17:04:26 +0530

    removed empty spaces

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-STACK_NAME=awsbootstrap 
+STACK_NAME=awsbootstrap
 REGION=ap-south-1
 CLI_PROFILE=awsbootstrap
+EC2_INSTANCE_TYPE=t2.micro
 
-EC2_INSTANCE_TYPE=t2.micro 
-
-# Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
@@ -15,5 +13,5 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \ 
+  --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE
