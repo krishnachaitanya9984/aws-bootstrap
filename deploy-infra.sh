8ce86db3da7b7b39b3ebe119c77fd29321191b6e
krinyny <krishnachaitanya9984@gmail.com>
2025-11-27 19:25:12 +0530

    changed stack name

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -30,7 +30,7 @@ echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME-main \
+  --stack-name $STACK_NAME \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
